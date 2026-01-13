0e7bb8ccaf33393a0f04168ca6363f15e15b9725
Jayesh Arora <jayesharora0001@gmail.com>
2026-01-13 21:41:04 +0530

    import cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-//import cors from "cors";
+import cors from "cors";
 import "dotenv/config";
 import { clerkMiddleware } from "@clerk/express";
 import aiRouter from "./routes/aiRoutes.js";
