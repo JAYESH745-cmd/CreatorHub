25b4bb2f7218aa1dfa9449f988c775c1252aa2ab
Jayesh Arora <jayesharora0001@gmail.com>
2026-01-13 21:20:44 +0530

    changes in cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 await connectCloudinary();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://creator-hub-nine.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
